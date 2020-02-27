2daa00857b4d1cc0dc56db19c388d2debd82ee03
shadow <773850645@qq.com>
2020-02-27 22:44:04 +0800

    2014

--- 2014/4/Employee.cpp
+++ 2014/4/Employee.cpp
@@ -24,7 +24,7 @@ void Employee::setSalary(int s) {
 	salary = s;
 }
 void Employee::extentHireDay(int y) {
-	if (deadline.getYear() + y - birthday.getYear() > 60)		// 年龄超过60
+	if (deadline.getYear() + y - birthday.getYear() > 60)				// 年龄超过60
 		cerr << "The age is too old." << endl;
 	else {
 		deadline.setDate(deadline.getYear() + y, deadline.getMonth(),	// 年龄合格
