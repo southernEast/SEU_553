b9b15e0afadf43becadea7190d2b6c1706d43ee5
shadow <773850645@qq.com>
2020-02-28 18:10:02 +0800

    2015

--- 2015/5/Teacher.cpp
+++ 2015/5/Teacher.cpp
@@ -16,29 +16,27 @@ Teacher::Teacher(const Teacher& t) {
 	birthday = t.birthday;
 	hireDate = t.hireDate;
 }
+
+// 以下两个函数有第二种设计方法，即不返回bool类型，而直接在函数内
+// 将需要输出的信息进行输出
 bool Teacher::isRetire(int year) const {
 	if (sex == "male")				
 		return (year - birthday.getYear() >= 55);		// 男性大于等于55岁退休
 	else
 		return (year - birthday.getYear() >= 60);		// 女性大于等于60岁退休
 }
+
+// 此函数只用于在满足isRetire的条件下的进一步判断
 bool Teacher::isKeepHire(int year) const {
 	if (isRetire(year) && sex == "female") {			// 如果满足退休年龄且是女性才需要合格第一条件
 		return (year - hireDate.getYear() < 35);		// 判断工龄是否达到35岁，未达到则续聘
-	}
+	}												
 	else
 		return false;
 }
 void Teacher::printInfo() const {
-	cout << "number: " << number << endl
-		<< "name: " << name << endl
-		<< "sex: " << sex << endl;
-	cout << "birthday: ";
-	birthday.printInfo();
-	cout << endl;
-	cout << "hire date: ";
-	hireDate.printInfo();
-	cout << endl;
+	cout << "name:" << name << endl
+		<< "number:" << number << endl << endl;
 }
 string Teacher::getName() const {
 	return name;
--- 2015/5/main.cpp
+++ 2015/5/main.cpp
@@ -8,17 +8,17 @@ int main() {
     Teacher t2(134, "Jack", "male", Date(1965,10, 10), Date(1995, 5, 3));
     Teacher t3(211, "Lucy", "female", Date(1955, 10, 10),Date(1982, 5, 3));
     Teacher t4(246, "Wendy", "female", Date(1955, 7, 9), Date(1980, 7, 31));
-    Teacher* t[4] = { &t1, &t2, &t3, &t4 };
+    Teacher* t[4] = { &t1, &t2, &t3, &t4 };     // 使用数组方便遍历
 
     cout << "Teachers need retire:\n";
     for (int i = 0; i < 4; ++i) {
         if (t[i]->isRetire(2016))
-            cout << t[i]->getName() << endl;
+            t[i]->printInfo();          // 输出姓名和编号信息
     }
     cout << "\nTeachers need keepHire:\n";
     for (int i = 0; i < 4; ++i) {
         if (t[i]->isRetire(2016) && t[i]->isKeepHire(2016))
-            cout << t[i]->getName() << endl;
+            t[i]->printInfo();
     }
 
 	return 0;
