5f344a3be9478edd912d90fa1c8aa6228cc8c239
shadow <773850645@qq.com>
2020-02-29 15:58:06 +0800

    2017

---
+++ 2017/1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
+int main() {
+	ifstream input("Date.txt", ios::in);
+	if (!input) {
+		cerr << "The file can not be opened." << endl;
+		exit(EXIT_FAILURE);
+	}			// 文件处理操作
+
+	int day, month, year;
+	const char* monthStr[] = {
+		"", "January", "February", "March", "April", "May", "June", "July",
+		"August", "September", "October", "November", "December"
+	};
+	while (input >> month) {	// 一直读取直到EOF
+		input.ignore();			// 忽略'/'
+		input >> day;
+		input.ignore();
+		input >> year;
+		input.ignore();
+
+		cout << monthStr[month] << " " << setw(2) 
+			<< setfill('0') << day << "," << year << endl;
+	}
+
+	input.close();
+
+	return 0;
+}
