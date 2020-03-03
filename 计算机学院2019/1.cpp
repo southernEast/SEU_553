58316a9deb4daf1b508e5d38ab641a15fd5956af
shadow <773850645@qq.com>
2020-03-03 21:21:43 +0800

    The school of computer science

---
+++ 计算机学院2019/1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+bool isRight(int a[][4], int m, int n) {
+	int val = a[m][n];
+
+	for (int i = 0; i < 3; ++i)	// 扫描第n列，如果有元素比val小说明val不是列最小
+		if (val > a[i][n])
+			return false;
+	for (int j = 0; j < 4; ++j)	// 扫描第m行，如果有元素比val大说明val不是行最大
+		if (val < a[m][j])
+			return false;
+	return true;
+}
+
+int main() {
+	int a[3][4];
+	srand(3);			// 初始化随机种子，选取其他的随机种子不一定会有鞍点
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			a[i][j] = 1 + rand() % 9;
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	cout << "\nThe points:\n";
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 4; ++j)
+			if (isRight(a, i, j))
+				cout << "(" << i << ","
+				<< j << ") " << a[i][j] << endl;
+	}
+
+	return 0;
+}
---
+++ 计算机学院2019/2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename T>
+int binarySearch(T a[], int left, int right, T elem) {
+	if (left <= right) {			// 如果查找范围仍有元素则继续查找
+		int mid = (left + right) / 2;	// 二分中点
+		if (a[mid] == elem)		// 找到元素
+			return mid;
+		else if (a[mid] < elem)	// 往右半边递归
+			binarySearch(a, mid + 1, right, elem);
+		else					// 往左半边递归
+			binarySearch(a, left, mid - 1, elem);
+	}
+	else			// 没有找到
+		return -1;
+}
+
+int main() {
+	int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int n = sizeof(a) / sizeof(a[0]);
+	int pos = binarySearch(a, 0, n - 1, 9);
+	cout << "Index: " << pos << "\nElement: " << a[pos];
+
+	return 0;
+}
