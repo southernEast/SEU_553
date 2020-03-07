abe84069d2fd9214e81f6020132ea81e0305d460
shadow <773850645@qq.com>
2020-03-07 21:30:31 +0800

    correct a sign

--- 2011/6/Tri.h
+++ 2011/6/Tri.h
@@ -25,7 +25,7 @@ public:
 	}
 	Tri(Point p1, Point p2, Point p3) : p1(p1), p2(p2), p3(p3) {}
 	virtual double calculate() const override {
-		return fabs(p1.x * p2.y + p2.x * p1.y + p3.x * p1.y
+		return fabs(p1.x * p2.y + p2.x * p3.y + p3.x * p1.y
 			- p1.x * p3.y - p2.x * p1.y - p3.x * p2.y) * 0.5;
 	}
 private:
