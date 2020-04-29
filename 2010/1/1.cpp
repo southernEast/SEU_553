9d06a9bc0b98ac0d90f9f1defe6146b96616b5ea
shadow <773850645@qq.com>
2020-04-29 16:32:09 +0800

    second

--- 2010/1/1.cpp
+++ 2010/1/1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 // 使用掩码进行转换
 void print(int num, ofstream& out) {
     const int SHIFT = sizeof(num) * 8 - 1;
-    const int MASK = (1 << SHIFT);				// 掩码
+    const int MASK = (1 << SHIFT);		// 掩码
 
     for (int i = 0; i < SHIFT + 1; ++i) {
         out << (num & MASK ? 1 : 0);        // 逐个取最高位
@@ -31,13 +31,16 @@ int main() {
         return -1;      // 文件打开失败
 
     srand(time(0));
-    int n = rand() % 20, num;
+    int n = rand() % 20;    // 获取0-19个数
     cout << n << endl;
     for (int i = 0; i < n; ++i) {
-        num = rand() % 256;
+        int num = rand() % 256; // 取八位二进制能够表示的随机数
+        outFile << num << endl; // 输出随机数的十进制
+
+        // 转换成二进制，两种方法
         print(num, outFile);
         transformBit(num, outFile);
-        outFile << "\n" << num << "\n\n";
+        outFile << "\n\n";
     }
 
     return 0;
