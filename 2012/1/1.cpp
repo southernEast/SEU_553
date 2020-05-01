1d4c23d8b3ae8e7d710f1a0100c542fa770b261f
shadow <773850645@qq.com>
2020-05-01 19:08:48 +0800

    second

--- 2012/1/1.cpp
+++ 2012/1/1.cpp
@@ -24,7 +24,7 @@ int main() {
 	int n1, n2, temp;
 	cin >> n1 >> n2;
 
-	temp = gcd(n1, n2);			// 求得最大公约数
+	temp = gcd(n1, n2);		// 求得最大公约数
 	cout << (n1 / temp * n2);	// 先除以最大公约数再乘，避免溢出
 
 	return 0;
--- 2012/2/2.cpp
+++ 2012/2/2.cpp
@@ -4,15 +4,14 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	cin.ignore();		// 忽略逗号
 
 	int sum = 0, temp;
 	for (int i = 0; i < n; ++i) {
-		cin >> temp;
 		cin.ignore();	// 忽略逗号
+		cin >> temp;
 		sum += temp;
 	}
 	cout << sum;
-	
+
 	return 0;
 }
