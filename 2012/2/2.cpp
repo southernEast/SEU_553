0218fba3402cab181fce493dff9902b28f8befbd
alextang <tangzf2001@126.com>
2023-04-05 11:23:15 +0800

    add_solution

--- 2012/2/2.cpp
+++ 2012/2/2.cpp
@@ -4,7 +4,13 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-
+	/*补充一个C语言的读法
+	while (n--) {
+        int x;
+        scanf(",%d", &x); 
+        sum += x;
+    }
+	*/
 	int sum = 0, temp;
 	for (int i = 0; i < n; ++i) {
 		cin.ignore();	// 忽略逗号
